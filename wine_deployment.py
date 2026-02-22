a315fc5d81805ba1c3c9c5d9d80b01f70c90c2ca
yashmane0502-stack <yashmane0502@gmail.com>
2026-02-22 11:32:02 +0530

    Update wine_deployment.py

--- wine_deployment.py
+++ wine_deployment.py
@@ -1,49 +1,41 @@
+
 import streamlit as st
 import joblib
 import pandas as pd
+model=joblib.load("dtc_model.pkl")
 
-# Load model
-model = joblib.load("dtc_model.pkl")
+st.title("wine type prediction !")
+fixed_acidity=st.number_input("fixed acidity")
+volatile_acidity=st.number_input("volatile acidity")
+citric_acid=st.number_input("citric acid")
+residual_sugar=st.number_input("residual sugar")
+chlorides=st.number_input("chlorides")
+free_sulfur_dioxide=st.number_input("free sulfur dioxide")
+total_sulfur_dioxide=st.number_input("total sulfur dioxide")
+density=st.number_input("density")
+pH=st.number_input("pH")
+sulphates=st.number_input("sulphates")
+alcohol=st.number_input("alcohol")
+quality=st.number_input("quality")
 
-st.title("Wine Type Prediction ")
+input_data=pd.DataFrame({
+    "fixed acidity":[fixed_acidity],
+    "volatile acidity":[volatile_acidity],
+    "citric acid":[citric_acid],
+    "residual sugar":[residual_sugar],
+    "chlorides":[chlorides],
+    "free sulfur dioxide":[free_sulfur_dioxide],
+    "total sulfur dioxide":[total_sulfur_dioxide],
+    "density":[density],
+    "pH":[pH],
+    "sulphates":[sulphates],
+    "alcohol":[alcohol],
+    "quality":[quality]
+})
 
-# Inputs
-fixed_acidity = st.number_input("fixed_acidity")
-volatile_acidity = st.number_input("volatile_acidity")
-citric_acid = st.number_input("citric_acid")
-residual_sugar = st.number_input("residual_sugar")
-chlorides  = st.number_input("chlorides")
-free_sulfur_dioxide = st.number_input("free_sulfur_dioxide")
-total_sulfur_dioxide = st.number_input("total_sulfur_dioxide")
-density = st.number_input("density")
-pH = st.number_input("pH")
-sulphates  = st.number_input("sulphates")
-alcohol = st.number_input("alcohol")
-
-# Create DataFrame (❌ removed "quality")
-input_data = pd.DataFrame([{
-    "fixed_acidity": fixed_acidity,
-    "volatile_acidity": volatile_acidity,
-    "citric_acid": citric_acid,
-    "residual_sugar": residual_sugar,
-    "chlorides": chlorides,
-    "free_sulfur_dioxide": free_sulfur_dioxide,
-    "total_sulfur_dioxide": total_sulfur_dioxide,
-    "density": density,
-    "pH": pH,
-    "sulphates": sulphates,
-    "alcohol": alcohol
-}])
-
-# Prediction
-if st.button("Predict"):
-    try:
-        prediction = model.predict(input_data)[0]
-
-        if prediction == "white":
-            st.success(" White Wine")
-        else:
-            st.error(" Red Wine")
-
-    except Exception as e:
-        st.error(f"Error: {e}")
+if st.button("predict"):
+  prediction=model.predict(input_data)[0]
+if prediction=="white":
+    st.success("white wine")
+else:
+    st.error("red wine")
